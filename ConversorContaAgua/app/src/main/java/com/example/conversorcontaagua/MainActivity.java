cd4824f51525c3e72cf962bbcf7b1475076b78da
RafaelDevMC <rafael@mcsistemas.eti.br>
2020-03-30 17:56:12 -0300

    Commit Inicial

---
+++ ConversorContaAgua/app/src/main/java/com/example/conversorcontaagua/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.conversorcontaagua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText valor = findViewById(R.id.editValorAguaSemTaxaServico);
+        valor.getText();
+
+
+    }
+}
